4fb37f6f305db20f076bfe0a46abfa07c0bb780b
Esteban <estebanarruin004@gmail.com>
2026-03-13 23:57:20 -0600

    Create three new resources to expand backend capabilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 import { ProvidersModule } from './providers/providers.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ManagersModule } from './managers/managers.module';
+import { LocationsModule } from './locations/locations.module';
+import { RegionsModule } from './regions/regions.module';
 
 
 @Module({
@@ -31,6 +34,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     EmployeesModule,
     ProductsModule,
     ProvidersModule,
+    ManagersModule,
+    LocationsModule,
+    RegionsModule,
   ],
 })
 export class AppModule {}
