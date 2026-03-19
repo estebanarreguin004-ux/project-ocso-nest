f50bfa97cf9b9f7f987f057a6d4a14a9dcb0d61e
Esteban <estebanarruin004@gmail.com>
2026-03-18 19:25:27 -0600

    Auth decorator implemented

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -7,8 +7,8 @@ import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@UseGuards(AuthGuard)
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
@@ -18,8 +18,7 @@ export class ProvidersController {
     return this.providersService.create(createProviderDto);
   }
 
-  @Roles(["Admin"])
-  @UseGuards(RolesGuard)
+  @Auth("Admin")
   @Get()
   findAll(@UserData() user: User) {
     console.log('Contenido del usuario en la request:', user);
